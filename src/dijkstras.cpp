a0023abc03fd5c90b2633abe99eea1ae62706cb1
Shizuka Takao <tkpp26@gmail.com>
2025-03-17 00:09:52 +0000

    revert dijkstras to original method

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -4,76 +4,76 @@
 #include <stack>
 using namespace std;
 
-// HW9 pseudocode
-vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-    int n = G.numVertices;
-    vector<int> distance(n, INF);
-    vector<bool> visited(n, false);
-    
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, source});
-    distance[source] = 0;
-    
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-        
-        if(visited[u])
-            continue;
-        else
-            visited[u] = true;
-        
-        for (Edge edge : G[u]) {
-            int v = edge.dst;
-            int weight = edge.weight;
-            
-            if (!visited[v] && distance[u] + weight < distance[v]) {
-                distance[v] = distance[u] + weight;
-                previous[v] = u;
-                pq.push({distance[v], v});
-            }
-        }
-    }
-    
-    return distance;
-}
-
-// Slides
+// HW9 pseudocode s
 // vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-//     int numV = G.numVertices;
-//     vector<int> distances(numV, INF);
-//     vector<bool> visited(numV, false);
-//     distances[source] = 0;
-//     previous[source] = -1; // acts as a stopping condition when calling extract_shortest_path or -1?
-
-//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap; // pair<vertex, weight>
-//     minHeap.push({source, 0}); // push source node to minHeap
-
-//     while(!minHeap.empty()) {
-//         int u = minHeap.top().first;
-//         minHeap.pop();
-
+//     int n = G.numVertices;
+//     vector<int> distance(n, INF);
+//     vector<bool> visited(n, false);
+    
+//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//     pq.push({0, source});
+//     distance[source] = 0;
+    
+//     while (!pq.empty()) {
+//         int u = pq.top().second;
+//         pq.pop();
+        
 //         if(visited[u])
 //             continue;
 //         else
 //             visited[u] = true;
-
-//         // G is an adjacency list so G[u] is a list of edges connected to u
+        
 //         for (Edge edge : G[u]) {
-//             int v = edge.dst; 
+//             int v = edge.dst;
 //             int weight = edge.weight;
-//             // Update shortest path to v from source
-//             // Update the previous node 
-//             // THINK : why is !visited[v] necessary?
-//             if(!visited[v] && distances[u] + weight < distances[v]) {
-//                 distances[v] = distances[u] + weight;
+            
+//             if (distance[u] + weight < distance[v]) {
+//                 distance[v] = distance[u] + weight;
 //                 previous[v] = u;
-//                 minHeap.push({v, distances[v]}); 
+//                 pq.push({distance[v], v});
 //             }
 //         }
 //     }
-//     return distances;
+    
+//     return distance;
 // }
+
+// Source : https://www.geeksforgeeks.org/priority-queue-in-cpp-stl/#
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int numV = G.numVertices;
+    vector<int> distances(numV, INF);
+    vector<bool> visited(numV, false);
+    distances[source] = 0;
+    previous[source] = -1; // acts as a stopping condition when calling extract_shortest_path or -1?
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap; // pair<vertex, weight>
+    minHeap.push({source, 0}); // push source node to minHeap
+
+    while(!minHeap.empty()) {
+        int u = minHeap.top().first;
+        minHeap.pop();
+
+        if(visited[u])
+            continue;
+        else
+            visited[u] = true;
+
+        // G is an adjacency list so G[u] is a list of edges connected to u
+        for (Edge edge : G[u]) {
+            int v = edge.dst; 
+            int weight = edge.weight;
+            // Update shortest path to v from source
+            // Update the previous node 
+            // THINK : why is !visited[v] necessary?
+            if(!visited[v] && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                previous[v] = u;
+                minHeap.push({v, distances[v]}); 
+            }
+        }
+    }
+    return distances;
+}
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     // Handle edge case : If path from source to dest doesn't exist
